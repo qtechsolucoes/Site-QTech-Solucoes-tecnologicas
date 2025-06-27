cdbc4207309da750e929c3b2b677d168cd8a3b95
QTech Soluções Tecnológicas <qtechsolucoestecnologicas@gmail.com>
2025-06-27 12:54:09 +0000

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./politica-de-privacidade.html",
+    "./termos-de-servico.html",
+    // Adicione outros arquivos como './assets/**/*.{js,ts,jsx,tsx}' se você tiver componentes JS/TS
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
